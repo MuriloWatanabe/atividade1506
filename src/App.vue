8aea49e14b9f65328ba41489f3b684b8a02449a0
murilowatanabe <muriloenzowatanabe14@gmail.com>
2022-06-15 09:30:55 -0300

    ok

---
+++ src/App.vue
@@ -0,0 +1,35 @@
+<script></script>
+<template>
+  <header class="menu-superior">
+    <div class="menu-esquerdo">
+      <span><RouterLink to="/">Home</RouterLink></span>
+      <span>|</span>
+      <span><RouterLink to="/about">Sobre</RouterLink></span>
+      <span>|</span>
+      <span><RouterLink to="/jogos">Jogos</RouterLink></span>
+    </div>
+  </header>
+  <main><router-view /></main>
+  <footer>rodape</footer>
+</template>
+
+<style>
+@import "@/assets/style.css";
+@import "@/assets/style.css";
+#app {
+  max-width: 1920px;
+  margin: auto;
+  height: 100vh;
+  font-weight: normal;
+}
+header,
+footer {
+  height: 15%;
+  display: flex;
+  align-items: center;
+  border: 1px black solid;
+}
+main {
+  height: 70%;
+}
+</style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,5 @@
+<script></script>
+
+<template>
+  <h1>oi</h1>
+</template>
